058bd4d304475f8be0b2806ba9df568a6e043a1f
callanhunter <callan57hunter@gmail.com>
2024-08-06 14:53:32 -0700

    added mongodb database but getting error

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 // express app
@@ -15,9 +16,23 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen for request
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000!!!", process.env.PORT);
-});
+// connect to db
 
-process.env;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for request
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000!k!!", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// listen for request
+// app.listen(process.env.PORT, () => {
+//   console.log("listening on port 4000!!!", process.env.PORT);
+// });
+
+// process.env;
